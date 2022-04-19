876b637cfbc09cb5e4376392de5c51ec8fd5702b
diablillo99lp <joan.hurtado.garcia@gmail.com>
2022-04-19 09:12:10 +0200

    commit

--- src/cine/mainApp.java
+++ src/cine/mainApp.java
@@ -4,19 +4,16 @@ public class mainApp {
 
 	public static void main(String[] args) {
 
-		
 		// TODO Auto-generated method stub
-			final int FILA = 8, COL = 9;
-				
-			// Sala de cine
-			char[][] sala = new char[FILA][COL];
+		final int FILA = 8, COL = 9;
 
-			
-			SalaCine cine = new SalaCine();
-			
-			cine.llenarMatriz(sala);
-			cine.mostrarCine(sala);
-		}
+		// Sala de cine
+		char[][] sala = new char[FILA][COL];
 
-		
+		SalaCine cine = new SalaCine();
+
+		cine.llenarMatriz(sala);
+		cine.mostrarCine(sala);
+	}
+
 }

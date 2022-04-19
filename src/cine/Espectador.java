d296f0f672cb34d7ea6fc3272a2579acf6691e4c
Daniel Fernandez Cacho <Dani.Fernandezcacho@gmail.com>
2022-04-19 10:16:41 +0200

    to string

--- src/cine/Espectador.java
+++ src/cine/Espectador.java
@@ -4,59 +4,65 @@ import java.util.Random;
 
 public class Espectador {
 
-		private String nombre;
-		private int edad;
-		private double dinero;
-		
-		public Espectador() {
-			this.nombre = "";
-			this.edad = 30;
-			this.dinero = 30.0;
+	private String nombre;
+	private int edad;
+	private double dinero;
+
+	public Espectador() {
+		this.nombre = "";
+		this.edad = 30;
+		this.dinero = 30.0;
+	}
+
+	public Espectador(String nombre, int edad, double dinero) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dinero = dinero;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public double getDinero() {
+		return dinero;
+	}
+
+	public void setDinero(double dinero) {
+		this.dinero = dinero;
+	}
+
+	// METODOS para ver si es apto de edad
+	public boolean aptoEdad(Pelicula peli) {
+		boolean apto = false;
+
+		if (getEdad() > peli.getEdadMinima()) {
+			apto = true;
 		}
-		
-		public Espectador(String nombre, int edad, double dinero) {
-			this.nombre = nombre;
-			this.edad = edad;
-			this.dinero = dinero;
+
+		return apto;
+	}
+
+	// metodo para ver si es comprar la entrada de cine
+	public boolean compraEntrada(SalaCine cine) {
+
+		if (cine.getPrecioEntrada() > getDinero()) {
+			return false;
+		} else {
+			return true;
 		}
-		
-		public String getNombre() {
-			return nombre;
-		}
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
-		public int getEdad() {
-			return edad;
-		}
-		public void setEdad(int edad) {
-			this.edad = edad;
-		}
-		public double getDinero() {
-			return dinero;
-		}
-		public void setDinero(double dinero) {
-			this.dinero = dinero;
-		}
-		
-		//METODOS para ver  si es apto de edad
-		public boolean aptoEdad(Pelicula peli) {
-			boolean apto = false;
-			
-			if (getEdad() > peli.getEdadMinima()) {
-	            apto = true;
-	        }
-			
-			return apto;
-		}
-		//metodo para ver si es comprar la entrada de cine
-		public boolean compraEntrada (SalaCine cine) {
-			
-			if (cine.getPrecioEntrada() > getDinero()) {
-				return false;
-			} else {
-				return true;
-			}
-		}
-				
+	}
+
 }
